72f9510669284bbac7807bd6a5d769d967c7669e
bgardell <bgardell@asu.edu>
2017-01-17 14:49:46 -0700

    User handler seperation

--- aspweb.py
+++ aspweb.py
@@ -3,6 +3,7 @@ from clingo import Control, parse_program
 
 import cherrypy
 import json
+from bson import json_util
 
 from termprocessor.processor import ModelProcessor, Term
 from utilities.userutil import UserHandler
@@ -11,7 +12,30 @@ from utilities.userutil import UserHandler
 class ASPWeb(object):
     terms = []
     mProcessor = ModelProcessor()
-    
+    userHandler = UserHandler()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def addTerm(self, user):
+        kbJson = cherrypy.request.json
+        try:
+            for term in kbJson["terms"]:
+                termAdded = Term()
+                termAdded.termFromJson(self.mProcessor.termDefinitions, term)
+                self.userHandler.addTerm(user, termAdded)
+            return { "Result" : "Success" }
+        except Exception as e:
+            return { "Result" : "Failed", "Reason" : e.message }
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def showTerms(self, user):
+        allTerms = json.dumps(self.userHandler.getAllTerms(user), default=json_util.default)
+        print allTerms
+        return allTerms
+
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
--- utilities/userutil.py
+++ utilities/userutil.py
@@ -3,11 +3,14 @@ from pymongo import MongoClient
 class UserHandler:
     def __init__(self):
         self.client = MongoClient()
-        self.termDb = client.terms
+        self.termDb = self.client.terms
 
     def addTerm(self, userName, term):
-        term_id = self.termDb[userName].insert_one(term).inserted_id
+        term_id = self.termDb[userName].insert_one(term.termJson).inserted_id
         return term_id
+
+    def getAllTerms(self, userName):
+        return self.termDb[userName]
 
 class User:
     name = ""
